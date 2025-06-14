d56856eeee2b61963ed8f820f15ba6748ff5c772
Harsh Shah <harshshah.coder@gmail.com>
2025-06-14 08:29:13 +0530

    Test Workflow

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    provideRouter(routes)
+  ]
 };
